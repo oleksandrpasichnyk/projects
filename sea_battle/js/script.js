6b9483214a3cd0b5dba38163b87c749ae35795ef
Oleksandr Pasichnyk <oleksandr.pasichnyk.work@gmail.com>
2020-03-06 23:47:51 +0200

    added random ships rendering and collision checking

--- sea_battle/js/script.js
+++ sea_battle/js/script.js
@@ -8,25 +8,79 @@ const fields = document.querySelectorAll('td');
 const header = document.querySelector('.header');
 
 const game = {
-    ships: [
-        {
-            location: ['26', '36', '46', '56'],
-            hits: ['', '', '', '']
-        },
-        {
-            location: ['11', '12', '13'],
-            hits: ['', '', '',]
-        },
-        {
-            location: ['69', '79'],
-            hits: ['', ''] 
-        },
-        {
-            location: ['32'],
-            hits: ['']
+    ships: [],
+    shipCount: 0,
+    optionShip: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1]
+    },
+    collision: new Set(),
+    generateShip(){
+        for(let i = 0; i < this.optionShip.count.length; i++){
+            for(let j = 0; j < this.optionShip.count[i]; j++){
+                const size = this.optionShip.size[i];
+                const ship = this.generateOptionsShip(size);
+                this.shipCount++;
+                this.ships.push(ship);
+            }
         }
-    ],
-    shipCount: 4
+    },
+    generateOptionsShip(shipSize){
+        const ship = {
+            hit: [],
+            location: []
+        }
+        let x, y;
+        const direction = Math.random() < 0.5;
+
+        if(direction){
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize)); 
+        }
+        else{
+            x = Math.floor(Math.random() * (10 - shipSize));
+            y = Math.floor(Math.random() * 10);
+        }
+
+        for(let i = 0; i < shipSize; i++){
+            if(direction){
+                ship.location.push(x + '' + (y + i));
+            }
+            else{
+                ship.location.push((x + i) + '' + y);
+            }
+            ship.hit.push('');
+        }
+
+        if(this.checkCollision(ship.location)){
+            return this.generateOptionsShip(shipSize);
+        }
+
+        this.addCollision(ship.location);
+        
+        return ship;
+    },
+    checkCollision(location){
+        for(const coord of location){
+            if(this.collision.has(coord)){
+                return true;
+            }
+        }
+    },
+    addCollision(location){
+        for(let i = 0; i < location.length; i++){
+            const startCoordX = location[i][0] - 1;
+            for(let j = startCoordX; j < startCoordX + 3; j++){
+                const startCoordY = location[i][1] - 1;
+                for(let z = startCoordY; z < startCoordY + 3; z++){
+                    if(j >= 0 && j < 10 && z >= 0 && z < 10){
+                        const coord = j + '' + z;
+                        this.collision.add(coord);
+                    }
+                }
+            }
+        }
+    }
 }
 
 const play = {
@@ -71,8 +125,8 @@ const fire = (event) => {
         if(index >=0){
             show.hit(target);
             play.updateData = 'hit';
-            ship.hits[index] = 'x';
-            const life = ship.hits.indexOf('');
+            ship.hit[index] = 'x';
+            const life = ship.hit.indexOf('');
             if(life < 0){
                 play.updateData = 'dead';
                 for (const id of ship.location) {
@@ -99,14 +153,10 @@ const fire = (event) => {
 const init = () => {
     enemy.addEventListener('click', fire);
     play.render();
-
+    game.generateShip();
     again.addEventListener('click', () => {
         location.reload();
     });
 };
 
 init(); 
-
-/*fields.forEach(field => {
-    field.addEventListener('click', fire);
-});*/
